44e29880920a5c9c2d20fccc1820c8029f3d4021
Alberto Sanchez <albertosanchez05@gmail.com>
2026-09-15 09:32:08 -0500

    feat(greetings): add developer greeting component

---
+++ src/greetings/presentation/developer-greeting.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+import {Developer} from "@/greetings/domain/model/developer.entity.js";
+import {computed} from "vue";
+
+const { developer } = defineProps({
+  developer: {
+    type: Developer,
+    required: true
+  },
+});
+
+const greeting = computed(() => {
+  return (developer && developer.isRegisterable())
+      ? `Congrats ${developer.fullName}!, identified by ID: ${developer.id}`
+      : 'Welcome Anonymous Developer!';
+});
+
+</script>
+
+<template>
+<p>{{ greeting }}</p>
+</template>
+
+<style scoped>
+
+</style>
